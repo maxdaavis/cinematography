02375a84a845c1aafc7510362253bc4cb8ac9925
maxdaavis <ausmaxd@gmail.com>
2024-05-27 23:09:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+// Close the modal when clicking outside of the modal content
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
